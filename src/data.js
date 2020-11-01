457beba46dabfa9832dd36a3405e96fa88a4c389
Teebreezy101 <tammy.batubo@gmail.com>
2020-11-01 03:28:34 +0100

    update data file with correct guinea pig sound

--- src/data.js
+++ src/data.js
@@ -1,55 +1,55 @@
 const Data = [
     {
         picture: 'https://i.postimg.cc/nhFd5QWt/cat-min.jpg',
-        id: 'Meow',
+        id: 'A cat meows',
         letter: 'Q',
         src: 'https://pet-sounds.s3.us-east-2.amazonaws.com/cat.mp3'
     },
     {
         picture: 'https://i.postimg.cc/mgmdGgx1/cow-min.jpg',
-        id: 'Moo',
+        id: 'A cow moos',
         letter: 'W',
         src: 'https://pet-sounds.s3.us-east-2.amazonaws.com/cow.mp3'
     },
     {
         picture: 'https://i.postimg.cc/d0D5QcvH/dog-min.jpg',
-        id: 'Bark',
+        id: 'A dog barks',
         letter: 'E',
         src: 'https://pet-sounds.s3.us-east-2.amazonaws.com/dog.mp3'
     },
     {
         picture: 'https://i.postimg.cc/W4KXhY12/dolphin-min.jpg',
-        id: 'Squeak',
+        id: 'A dolphin squeaks',
         letter: 'A',
         src: 'https://pet-sounds.s3.us-east-2.amazonaws.com/dolphin.mp3'
     },
     {
         picture: 'https://i.postimg.cc/MHJ5YcBN/guineapig-min.jpg',
-        id: 'Click',
+        id: 'A guinea pig wheeks',
         letter: 'S',
         src: 'https://pet-sounds.s3.us-east-2.amazonaws.com/guineapig.mp3'
     },
     {
         picture: 'https://i.postimg.cc/ZKcHr55s/horse-min.jpg',
-        id: 'Neigh',
+        id: 'A horse neighs',
         letter: 'D',
         src: 'https://pet-sounds.s3.us-east-2.amazonaws.com/horse.mp3'
     },
     {
         picture: 'https://i.postimg.cc/Rh1RpMQF/parrot-min.jpg',
-        id: 'Squawk',
+        id: 'A parrot squawks',
         letter: 'Z',
         src: 'https://pet-sounds.s3.us-east-2.amazonaws.com/parrot.mp3'
     },
     {
         picture: 'https://i.postimg.cc/K88NQhKG/rooster-min.jpg',
-        id: 'Crow',
+        id: 'A rooster crows',
         letter: 'X',
         src: 'https://pet-sounds.s3.us-east-2.amazonaws.com/rooster.mp3'
     },
     {
         picture: 'https://i.postimg.cc/dQG4VBPY/sheep-min.jpg',
-        id: 'Bleat',
+        id: 'A sheep bleats',
         letter: 'C',
         src: 'https://pet-sounds.s3.us-east-2.amazonaws.com/sheep.mp3'
     }
